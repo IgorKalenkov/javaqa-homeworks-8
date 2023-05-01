ccb4d9dc6a347b0bc45dd2ba3ddd1af81c6dec15
IgorKalen <i15000@mail.ru>
2023-05-01 23:15:48 +0300

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public FormDate birthday;
+    public boolean subscription;
+}
